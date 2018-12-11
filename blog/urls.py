0fbe44872c38fecfe27cb25a2e3ee30183862509
Paul Louis <iampaullouis@gmail.com>
2018-12-11 17:03:34 +0530

    Starting forms

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path,include
-from .views import BlockListView,BlogDetailView
+from .views import BlockListView,BlogDetailView,BlogCreateView
 
 urlpatterns=[
 	path('',BlockListView.as_view(),name='home'),
-	path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail')
+	path('post/<int:pk>/',BlogDetailView.as_view(),name='post_detail'),
+	path('post/new/',BlogCreateView.as_view(),name='post_new'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Post
+from django.views.generic.edit import CreateView
 # Create your views here.
 
 class BlockListView(ListView):
@@ -10,4 +11,11 @@ class BlockListView(ListView):
 class BlogDetailView(DetailView):
 	model=Post
 	template_name='post_detail.html'
+
+class BlogCreateView(CreateView):
+	model=Post
+	template_name='post_new.html'
+	fields=['title','author','body']
+
+
 
