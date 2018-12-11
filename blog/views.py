b30257241d710d0cccc7149420580d592c293b09
Paul Louis <iampaullouis@gmail.com>
2018-12-11 19:04:42 +0530

    CRUD complete and added tests

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.views.generic import ListView,DetailView
 from .models import Post
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
 # Create your views here.
 
 class BlockListView(ListView):
@@ -17,5 +18,14 @@ class BlogCreateView(CreateView):
 	template_name='post_new.html'
 	fields=['title','author','body']
 
+class BlogUpdateView(UpdateView):
+	model=Post
+	template_name='post_edit.html'
+	fields=['title','body']
+
+class BlogDeleteView(DeleteView):
+	model=Post
+	template_name='post_delete.html'
+	success_url=reverse_lazy('home')
 
 
